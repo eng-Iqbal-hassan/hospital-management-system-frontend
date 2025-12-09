e9be80791f2df07f93cc3135ce5ace48e8d39855
eng-Iqbal-hassan <eng.iqbal110@gmail.com>
2025-12-09 23:53:16 +0500

    get profile feature is added

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -11,6 +11,7 @@ const AppContextProvider = (props) => {
     const currencySymbol = "$";
     const backendUrl = import.meta.env.VITE_BACKEND_URL; 
     const [doctors, setDoctors] = useState([]);
+    const [userData, setUserData] = useState(false);
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false);
 
     const getDoctorsData = async () => {
@@ -27,15 +28,41 @@ const AppContextProvider = (props) => {
         }
     }
 
+    const loadUserProfileData = async () => {
+        try {
+           const {data} = await axios.get(backendUrl + '/api/user/get-profile',{
+            headers: {token}
+           }) 
+           if(data.success) {
+            setUserData(data.userData);
+           } else {
+            toast.error(data.message)
+           }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
     const value= {
         doctors, currencySymbol, 
         token, setToken,
-        backendUrl
+        backendUrl,
+        userData, setUserData,
+        loadUserProfileData
     }
 
     useEffect(()=>{
         getDoctorsData();
     },[])
+
+    useEffect(()=>{
+        if(token) {
+            loadUserProfileData();
+        } else {
+            setUserData(false)
+        }
+    },[token])
 
     return (
         <AppContext.Provider value={value}>
